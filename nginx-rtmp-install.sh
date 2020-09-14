da7a8cad4adcd3dd285d78af28c9af92919d6037
qian2501 <qian2501@gmail.com>
2020-09-14 13:31:20 +0800

    modify nginx-rtmp-install for some real env fail

--- nginx-rtmp-install.sh
+++ nginx-rtmp-install.sh
@@ -14,12 +14,13 @@ OS=${OS%\"*}
 # Get package manager from distro
 if [ $OS = "ubuntu" ]||[ $OS = "debian" ]; then
     PM=apt
-    PKGS="ca-certificates openssl libssl-dev libpcre3-dev"
+    PKGS="gcc make ca-certificates openssl libssl-dev libpcre3-dev"
 elif [ $OS = "centos" ]||[ $OS = "rhel" ]; then
     PM=yum
-    PKGS="ca-certificates openssl openssl-devel pcre-devel"
+    PKGS="gcc make ca-certificates openssl openssl-devel pcre-devel"
 fi
 
+# Install required packages
 echo $SEP
 $PM update
 $PM install -y $PKGS
@@ -28,20 +29,38 @@ $PM install -y $PKGS
 echo $SEP
 mkdir -p /tmp/build/nginx
 cd /tmp/build/nginx
-wget -O $NGINX_VERSION.tar.gz https://nginx.org/download/$NGINX_VERSION.tar.gz
-tar -xzf $NGINX_VERSION.tar.gz
+if [ ! -d /tmp/build/nginx/$NGINX_VERSION ]; then
+    wget -O $NGINX_VERSION.tar.gz https://nginx.org/download/$NGINX_VERSION.tar.gz
+    tar -xzf $NGINX_VERSION.tar.gz
+fi
 
 # Download and decompress RTMP module
 echo $SEP
 mkdir -p /tmp/build/nginx-rtmp-module
 cd /tmp/build/nginx-rtmp-module
-wget -O nginx-rtmp-module-$NGINX_RTMP_MODULE_VERSION.tar.gz https://github.com/arut/nginx-rtmp-module/archive/v$NGINX_RTMP_MODULE_VERSION.tar.gz
-tar -zxf nginx-rtmp-module-$NGINX_RTMP_MODULE_VERSION.tar.gz
+if [ ! -d /tmp/build/nginx-rtmp-module/nginx-rtmp-module-$NGINX_RTMP_MODULE_VERSION ]; then
+    wget -O nginx-rtmp-module-$NGINX_RTMP_MODULE_VERSION.tar.gz https://github.com/arut/nginx-rtmp-module/archive/v$NGINX_RTMP_MODULE_VERSION.tar.gz
+    tar -zxf nginx-rtmp-module-$NGINX_RTMP_MODULE_VERSION.tar.gz
+fi
+
+# In case source download failed
+if [ ! -d /tmp/build/nginx/$NGINX_VERSION ]||[ ! -d /tmp/build/nginx-rtmp-module/nginx-rtmp-module-$NGINX_RTMP_MODULE_VERSION ]; then
+    exit 0
+fi
+
+# Patch for current version rtmp module compile warning
+sed -i '169i /* fall through */' /tmp/build/nginx-rtmp-module/nginx-rtmp-module-$NGINX_RTMP_MODULE_VERSION/ngx_rtmp_eval.c
 
 # Build and install Nginx
 # The default puts everything under /usr/local/nginx, so it's needed to change explicitly.
 # Not just for order but to have it in the PATH
 echo $SEP
+if [ ! -d /sbin/nginx ]; then
+    mkdir /sbin/nginx
+fi
+if [ ! -d /etc/nginx ]; then
+    mkdir /etc/nginx
+fi
 cd /tmp/build/nginx/$NGINX_VERSION
 ./configure \
     --sbin-path=/sbin/nginx \
