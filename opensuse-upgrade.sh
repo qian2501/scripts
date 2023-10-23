2e6a1c234df719d113cf6e4d6c4df590caee734f
qian2501 <qian2501@gmail.com>
2023-10-23 10:17:43 +0800

    version

--- opensuse-upgrade.sh
+++ opensuse-upgrade.sh
@@ -7,6 +7,6 @@ sudo zypper update
 
 #sudo sed -i 's/15.4/${releasever}/g' /etc/zypp/repos.d/*.repo
 
-sudo zypper --releasever=15.5 refresh
+sudo zypper --releasever=$VERSION refresh
 
-sudo zypper --releasever=15.5 dup --download-in-heaps
+sudo zypper --releasever=$VERSION dup --download-in-heaps
